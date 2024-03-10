59e42e868c4e122ab850b7220310e30dbabc651f
Leonardo Toledo <leotoledo010@gmail.com>
2024-03-10 11:14:49 -0300

    atualizacao banco de dados

--- sakura/database/migrations/2024_03_09_235227_create_sakura_table.php
+++ sakura/database/migrations/2024_03_09_235227_create_sakura_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         $this->createTableIfNotExists('sakura', function (Blueprint $table) {
-            // Funcionarios
+            // Funcionários
             $table->id('FuncionarioID');
             $table->string('Nome');
             $table->string('Cargo');
@@ -21,31 +21,43 @@ return new class extends Migration
             $table->timestamps();
         });
 
+        // Categorias
+        $this->createTableIfNotExists('categorias', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->timestamps();
+        });
+        // Pratos
         $this->createTableIfNotExists('pratos', function (Blueprint $table) {
             $table->id('PratoID');
             $table->string('NomePrato');
+            $table->string('Foto')->nullable();
             $table->text('DescricaoPrato');
             $table->decimal('PrecoPrato', 10, 2);
-            $table->string('CategoriaPrato');
+            $table->foreignId('categoria_id')->constrained('categorias')->onUpdate('cascade')->onDelete('cascade');
             $table->timestamps();
         });
-
+        // Bebibas
         $this->createTableIfNotExists('bebidas', function (Blueprint $table) {
             $table->id('BebidaID');
             $table->string('NomeBebida');
+            $table->string('Foto')->nullable();
             $table->text('DescricaoBebida');
             $table->decimal('PrecoBebida', 10, 2);
+            $table->foreignId('categoria_id')->constrained('categorias')->onUpdate('cascade')->onDelete('cascade');
             $table->timestamps();
         });
-
+        // Sobremesa
         $this->createTableIfNotExists('sobremesas', function (Blueprint $table) {
             $table->id('SobremesaID');
             $table->string('NomeSobremesa');
+            $table->string('Foto')->nullable();
             $table->text('DescricaoSobremesa');
             $table->decimal('PrecoSobremesa', 10, 2);
+            $table->foreignId('categoria_id')->constrained('categorias')->onUpdate('cascade')->onDelete('cascade');
             $table->timestamps();
         });
-
+        //Mesas 
         $this->createTableIfNotExists('mesas', function (Blueprint $table) {
             $table->id('MesaID');
             $table->integer('NumeroMesa');
@@ -53,6 +65,7 @@ return new class extends Migration
             $table->string('StatusMesa');
             $table->timestamps();
         });
+        // Emails 
         $this->createTableIfNotExists('emails', function (Blueprint $table) {
             $table->id('EmailID');
             $table->string('RemetenteEmail');
@@ -63,6 +76,7 @@ return new class extends Migration
             $table->string('StatusEmail');
             $table->timestamps();
         });
+        // Reservas
         $this->createTableIfNotExists('reservas', function (Blueprint $table) {
             $table->id('ReservaID');
             $table->string('ClienteReserva');
@@ -71,12 +85,12 @@ return new class extends Migration
             $table->time('HoraReserva');
             $table->integer('NumeroPessoasReserva');
             $table->string('StatusReserva');
-            $table->foreignId('MesaIDReserva')->constrained('mesas');
-            $table->foreignId('EmailIDReserva')->constrained('emails');
+            $table->foreignId('MesaIDReserva')->constrained('mesas', 'MesaID')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('EmailIDReserva')->constrained('emails', 'EmailID')->onUpdate('cascade')->onDelete('cascade');
             $table->timestamps();
         });
 
-
+        //Faturamento
         $this->createTableIfNotExists('faturamento', function (Blueprint $table) {
             $table->id('FaturaID');
             $table->date('DataFatura');
@@ -91,14 +105,15 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('sakura');
-        Schema::dropIfExists('pratos');
-        Schema::dropIfExists('bebidas');
-        Schema::dropIfExists('sobremesas');
-        Schema::dropIfExists('mesas');
-        Schema::dropIfExists('reservas');
-        Schema::dropIfExists('faturamento');
         Schema::dropIfExists('emails');
+        Schema::dropIfExists('faturamento');
+        Schema::dropIfExists('reservas');
+        Schema::dropIfExists('mesas');
+        Schema::dropIfExists('sobremesas');
+        Schema::dropIfExists('bebidas');
+        Schema::dropIfExists('pratos');
+        Schema::dropIfExists('categorias');
+        Schema::dropIfExists('sakura');
     }
 
     /**
@@ -108,7 +123,7 @@ return new class extends Migration
      * @param  \Closure  $callback
      * @return void
      */
-    private function createTableIfNotExists(string $tableName, Closure $callback): void
+    private function createTableIfNotExists(string $tableName, \Closure $callback): void
     {
         if (!Schema::hasTable($tableName)) {
             Schema::create($tableName, $callback);
