9c840691365a1d1e1c5826e960166988d6e127e6
Leonardo Toledo <leotoledo010@gmail.com>
2024-03-12 08:13:18 -0300

    heranca

--- sakura/resources/views/pages/banner.blade.php
+++ sakura/resources/views/pages/banner.blade.php
@@ -1,11 +1,11 @@
-@extends('layouts.app')
+@extends('header')
 @section('banner')
 
 <section class="banner">
-    <img src="{{ asset('images/cinza.png') }}"alt="">
+    <img src="{{ asset('images/cinza.png') }}" alt="">
     <img src="{{ asset('images/preto.png') }}" alt="">
-    <img src="{{ asset('images/cinza.png') }}"alt="">
+    <img src="{{ asset('images/cinza.png') }}" alt="">
     <img src="{{ asset('images/preto.png') }}" alt="">
 </section>
-    
+
 @endsection
--- sakura/resources/views/pages/header.blade.php
+++ sakura/resources/views/pages/header.blade.php
@@ -1,32 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-    <header>
-        <div class="site menu-header">
-            <div class="logo">
-                <img src="{{ asset('images/sakura.svg') }}" alt="">
-            </div>
-            <nav class="lista-menu">
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Sobre</a></li>
-                    <li><a href="#">Menu</a></li>
-                    <li><a href="#">Reserva</a></li>
-                    <li><a href="#">Contato</a></li>
-                </ul>
-            </nav>
-            <div class="tema">
-                <img class="lua" src="{{ asset('images/lua.png') }}" alt="">
-                <img class="sol" src="{{ asset('images/sol.png') }}" alt="">
-            </div>
+<header>
+    <div class="site menu-header">
+        <div class="logo">
+            <img src="{{ asset('images/sakura.svg') }}" alt="">
         </div>
-    </header>
-    <section class="banner">
-        <img src="{{ asset('images/cinza.png') }}"alt="">
-        <img src="{{ asset('images/preto.png') }}" alt="">
-        <img src="{{ asset('images/cinza.png') }}"alt="">
-        <img src="{{ asset('images/preto.png') }}" alt="">
-    </section>
+        <nav class="lista-menu">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Menu</a></li>
+                <li><a href="#">Reserva</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+        <div class="tema">
+            <img class="lua" src="{{ asset('images/lua.png') }}" alt="">
+            <img class="sol" src="{{ asset('images/sol.png') }}" alt="">
+        </div>
+    </div>
+</header>
 
-  
+@yield('banner')
+
+
 @endsection
