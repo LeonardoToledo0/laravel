dc8b9841420520b798526e1e012abbc4a6462531
Leonardo Toledo <leotoledo010@gmail.com>
2024-03-12 17:39:00 -0300

    Atualizacao Sobre

--- sakura/resources/views/layouts/app.blade.php
+++ sakura/resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,7 +15,7 @@
     <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('favicon/apple-icon-144x144.png') }}">
     <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('favicon/apple-icon-152x152.png') }}">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-icon-180x180.png') }}">
-    <link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('favicon/android-icon-192x192.png') }}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('favicon/android-icon-192x192.png') }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
     <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('favicon/favicon-96x96.png') }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
@@ -24,17 +25,19 @@
     <link rel="stylesheet" href="{{ asset('css/slick.css') }}">
     <link rel="stylesheet" href="{{ asset('css/slick-theme.css') }}">
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
-   
-
-    <title>@yield('title')Sakura-Sushi </title>
-
+    {{-- Title --}}
+    <title>@yield('title') Sakura-Sushi </title>
 </head>
+
 <body>
-    
-        @yield('content')
-        
-<script src="{{ asset('https://code.jquery.com/jquery-3.7.1.min.js') }}" ></script>  
-<script src="{{ asset('js/slick.min.js') }}" ></script> 
-<script src="{{ asset('js/animacoes.js') }}" ></script>
+
+    @yield('content')
+
+
+    <!-- Seus scripts JS aqui -->
+    <script src="{{ asset('https://code.jquery.com/jquery-3.7.1.min.js') }}"></script>
+    <script src="{{ asset('js/slick.min.js') }}"></script>
+    <script src="{{ asset('js/animacoes.js') }}"></script>
 </body>
+
 </html>
---
+++ sakura/resources/views/pages/home.blade.php
@@ -0,0 +1,59 @@
+<!-- pages.home.blade.php -->
+
+@extends('layouts.app')
+
+@section('content')
+{{-- Inicio do Header --}}
+<header>
+    <div class="site menu-header">
+        <div class="logo">
+            <img src="{{ asset('images/sakura.svg') }}" alt="">
+        </div>
+        <nav class="lista-menu">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Menu</a></li>
+                <li><a href="#">Reserva</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+        <div class="tema">
+            <img class="lua" src="{{ asset('images/lua.png') }}" alt="">
+            <img class="sol" src="{{ asset('images/sol.png') }}" alt="">
+        </div>
+    </div>
+</header>
+
+{{-- Inicio Banner --}}
+<section class="banner">
+    <img src="{{ asset('images/cinza.png') }}" alt="">
+    <img src="{{ asset('images/preto.png') }}" alt="">
+    <img src="{{ asset('images/cinza.png') }}" alt="">
+    <img src="{{ asset('images/preto.png') }}" alt="">
+</section>
+
+{{--Inicio Sobre --}}
+<section class="sobre">
+    <div class=" site sobre-titulo">
+        <hr class="h1">
+        <h2>Sobre</h2>
+        <hr class="h2">
+    </div>
+    <div class=" site sobre-content">
+        <div class="sobre-texto"><img src="{{ asset('images/fundocomida.png') }}" alt="">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro aliquam adipisci totam quia, sunt nemo
+                sed velit quis ea suscipit magni iste cumque, necessitatibus voluptatum, non impedit. Voluptates, ipsam
+                molestias?</p><a href="#">Ver Mais</a>
+        </div>
+        <div class="sobre-comida"><img src="{{ asset('images/comida.png') }}" alt=""></div>
+        <div><img src="{{ asset('images/comida.png') }}" alt=""></div>
+    </div>
+    <div class="sobre-faixa"></div>
+
+
+</section>
+
+
+
+@endsection
